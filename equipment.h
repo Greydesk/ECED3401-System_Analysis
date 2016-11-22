2c11bd0791734ff3e5c42b976af2dc4e5fd08d54
statik <statik@Statik-Laptop>
2016-11-21 23:30:08 -0400

    Initial work

--- equipment.h
+++ equipment.h
@@ -1,1 +1,12 @@
 // Includes equipment class hierarchy and functions
+class equipment {
+	friend class bakery;
+	private:
+		int _id;						// Unique ID number, sequentially designated
+
+	public:
+		equipment(int id); 				// Ctor
+		int  id();						// Getter: ID
+		void write(ostream &out);		// Write function for debugging
+		void write_rpt(ostream &out);	// Special write function for customer rpt
+};
