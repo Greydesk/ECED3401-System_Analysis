22ae3ec8768686aa33a2e70b0df2eb99b8ba7e01
CPMuise <Christian.Muise@dal.ca>
2016-11-22 20:51:48 -0400

    Update bakery.h

--- bakery.h
+++ bakery.h
@@ -4,6 +4,7 @@
 /*************************************************** BAKERY CLASS *****************************************************/
 /**********************************************************************************************************************/
 #include <iostream>
+#include <fstream>
 #include "customer.h"
 #include "food.h"
 #include "equipment.h"
@@ -35,9 +36,9 @@ class bakery {
 		
 		void orderKits(int nCakes, int nCrois); // Removes funds, adds items to foodMaster
 
-		void writeRpt_daily(ostream &out);    // 
-		void writeRpt_customer(ostream &out, const &list<fooditem> _customerMaster);
-		void writeRpt_food(ostream &out, const &list<fooditem> _foodMaster);
+		void writeRpt_daily(ofstream &out);    // daily report
+		void writeRpt_customer(ofstream &out, const &list<fooditem> _customerMaster);
+		void writeRpt_food(ofstream &out, const &list<fooditem> _foodMaster);
 }
 
 bakery(float cakePrice, float croissantPrice, list<customer>* cMaster, list<foodItem>* fMaster)
@@ -71,6 +72,16 @@ void deposit() // Add deposit ammount to cumulative cash flow
 
 void withdraw(float wthAmt); // Subtract deposit ammount from cumulative cash flow
 
-void report_daily(ostream &out);
-void report_customer(ostream &out);
-void report_food(ostream &out);
+void report_daily(ofstream &out)
+{
+	_tally.writeRpt(out);
+}
+
+void report_customer(ofstream &out)
+{
+	
+}
+void report_food(ofstream &out)
+{
+	
+}
