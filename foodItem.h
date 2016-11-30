9c709b624562642e9d6c477c1b667b791caec8c5
CPMuise <Christian.Muise@dal.ca>
2016-11-30 07:23:51 -0400

    Added things in desparation
    See above

--- foodItem.h
+++ foodItem.h
@@ -7,210 +7,213 @@
 /*
 	Notes:
 		- Much information can be inferred form the variable values
-			- ((cID is set)^(time + prepStart )) --> ()
+			- ((customer_id is set)^(time + prepStart)) --> ()
 			- () --> ()
 */
+
 #include <iostream>
 
 using namespace std;
 
-#define HOLDTIME 15
+#define HOLD_TIME 15
 /******************************************************************************/
 /***************************** CLASS STRUCTURE ********************************/
 /******************************************************************************/
 
-class foodItem{
+class FoodItem{
 	private:
-		int _id;          // Sequential, unique ID indicating 
-		int _fType;       // Type of food item: 0 = cake, 1 = croissant
-		int _orderT;      // Time when item (kit) was ordered, -1 if NAN
-		int _delivT;      // Time when item (kit) was delivered, -1 if NAN
-		int _prepStrT;    // Time when prep began, -1 if NAN
-		int _prepEndT;    // Time when prep ended, -1 if NAN
-		int _relinquishT; // Time when item was either sold/discarded, -1 if NAN
-		int _status;      // -1 = default, 0 = discarded, 1 = sold
-		int _cID;         // ID of customer who holds claim over item , -1 by default/discarded
+		int _food_id;         // Sequential, unique ID indicating 
+		int _food_type;       // Type of food item: 0 = cake, 1 = croissant
+		int _order_time;      // Time when item (kit) was ordered, -1 if NAN
+		int _delivery_time;   // Time when item (kit) was delivered, -1 if NAN
+		int _prep_start_time; // Time when prep began, -1 if NAN
+		int _prep_end_time;   // Time when prep ended, -1 if NAN
+		int _relinquish_time; // Time when item was either sold/discarded, -1 if NAN
+		int _status;          // -1 = default, 0 = discarded, 1 = sold
+		int _customer_id;     // ID of customer who holds claim over item , -1 by default/discarded
 
 	public:
 		// Ctors
-		foodItem(int id, int fType); // For initial stock
-		foodItem(int id, int fType, int orderT, int _delivT); // For ordered stock w/o customer req
-		foodItem(int id, int fType, int orderT, int _delivT, int cID); // For ordered stock w/ customer req
+		FoodItem(int food_id, int food_type); // For initial stock
+		FoodItem(int food_id, int food_type, int order_time, int _delivery_time); // For ordered stock w/o customer req
+		FoodItem(int food_id, int food_type, int order_time, int _delivery_time, int customer_id); // For ordered stock w/ customer req
 		// Getters
-		int id();          // Getter: ID     of food item
-		int fType();       // Getter: Type   of food item
-		int orderT();      // Getter: Time   of kit order
-		int delivT();      // Getter: Time   of kit delivery
-		int prepStrT();    // Getter: Time   of food prep start
-		int prepEndT();    // Getter: Time   of food prep end
-		int relinquishT(); // Getter: Time   of food item relinquishing
-		int status();      // Getter: Status of food item
-		int cID();         // Getter: ID     of customer holding claim over item
+		int food_id();         // Getter: ID     of food item
+		int food_type();       // Getter: Type   of food item
+		int order_time();      // Getter: Time   of kit order
+		int delivery_time();   // Getter: Time   of kit delivery
+		int prep_start_time(); // Getter: Time   of food prep start
+		int prep_end_time();   // Getter: Time   of food prep end
+		int relinquish_time(); // Getter: Time   of food item relinquishing
+		int status();          // Getter: Status of food item
+		int customer_id();     // Getter: ID     of customer holding claim over item
 		// Setters
-		void orderT(int orderT);           // Setter: Time   of kit order
-		void delivT(int delivT);           // Setter: Time   of kit delivery
-		void prepStrT(int prepStrT);       // Setter: Time   of food prep start
-		void prepEndT(int prepEndT);       // Setter: Time   of food prep end
-		void relinquishT(int relinquishT); // Setter: Time   of food item relinquishing
-		void status(int status);           // Setter: Status of food item
-		void cID(int cID);               // Setter: ID     of customer holding claim over item
+		void order_time(int order_time);           // Setter: Time   of kit order
+		void delivery_time(int delivery_time);     // Setter: Time   of kit delivery
+		void prep_start_time(int prep_start_time); // Setter: Time   of food prep start
+		void prep_end_time(int prep_end_time);     // Setter: Time   of food prep end
+		void relinquish_time(int relinquish_time); // Setter: Time   of food item relinquishing
+		void status(int status);                   // Setter: Status of food item
+		void customer_id(int customer_id);         // Setter: ID     of customer holding claim over item
 		// Descriptive Functions
-		int isReserved(void); // returns customer ID if reserved, -1 otherwise
+		int  IsReserved(void); // returns customer ID if reserved, -1 otherwise
 		// Write Functions
-		void write(ostream &out);     // General write method
-		void write_Rpt(ostream &out); // Specialized write method for food report
+		void Write(ostream &out);       // General write method
+		void WriteReport(ostream &out); // Specialized write method for food report
 };
 
 /******************************************************************************/
 /********************************** CTOR **************************************/
 /******************************************************************************/
 
-foodItem::foodItem(int id, int fType) // For initial stock
+FoodItem::FoodItem(int food_id, int food_type) // For initial stock
 {
-	_id          = id;
-	_fType       = fType;
-	_orderT      = -1;
-	_delivT      = -1;
-	_prepStrT    = -1;
-	_prepEndT    = -1;
-	_relinquishT = -1;
-	_status      = -1;
-	_cID         = -1;
+	_food_id         = food_id;
+	_food_type       = food_type;
+	_order_time      = -1;
+	_delivery_time   = -1;
+	_prep_start_time = -1;
+	_prep_end_time   = -1;
+	_relinquish_time = -1;
+	_status          = -1;
+	_customer_id     = -1;
 }
 
-foodItem::foodItem(int id, int fType, int orderT, int delivT) // For ordered stock w/o customer req
+FoodItem::FoodItem(int food_id, int food_type, int order_time, int delivery_time) // For ordered stock w/o customer req
 {
-	_id          = id;
-	_fType       = fType;
-	_orderT      = orderT;
-	_delivT      = delivT;
-	_prepStrT    = -1;
-	_prepEndT    = -1;
-	_relinquishT = -1;
-	_status      = -1;
-	_cID         = -1;
+	_food_id         = food_id;
+	_food_type       = food_type;
+	_order_time      = order_time;
+	_delivery_time   = delivery_time;
+	_prep_start_time = -1;
+	_prep_end_time   = -1;
+	_relinquish_time = -1;
+	_status          = -1;
+	_customer_id     = -1;
 }
 
-foodItem::foodItem(int id, int fType, int orderT, int delivT, int cID) // For ordered stock w customer req
+FoodItem::FoodItem(int food_id, int food_type, int order_time, int delivery_time, int customer_id) // For ordered stock w customer req
 {
-	_id          = id;
-	_fType       = fType;
-	_orderT      = orderT;
-	_delivT      = delivT;
-	_prepStrT    = -1;
-	_prepEndT    = -1;
-	_relinquishT = -1;
-	_status      = -1;
-	_cID         = cID;
+	_food_id         = food_id;
+	_food_type       = food_type;
+	_order_time      = order_time;
+	_delivery_time   = delivery_time;
+	_prep_start_time = -1;
+	_prep_end_time   = -1;
+	_relinquish_time = -1;
+	_status          = -1;
+	_customer_id     = customer_id;
 }
 
 /******************************************************************************/
 /********************************* GETTERS ************************************/
 /******************************************************************************/
 
-int foodItem::id()
+int FoodItem::food_id()
 {
-	return _id;
+	return _food_id;
 }
 
-int foodItem::fType()
+int FoodItem::food_type()
 {
-	return _fType;
+	return _food_type;
 }
 
-int foodItem::orderT(){
-	return _orderT;
-}
-
-int foodItem::delivT()
+int FoodItem::order_time()
 {
-	return _delivT;
+	return _order_time;
 }
 
-int foodItem::prepStrT()
+int FoodItem::delivery_time()
 {
-	return _prepStrT;
+	return _delivery_time;
 }
 
-int foodItem::prepEndT()
+int FoodItem::prep_start_time()
 {
-	return _prepStrT;
+	return _prep_start_time;
 }
 
-int foodItem::relinquishT()
+int FoodItem::prep_end_time()
 {
-	return _relinquishT;
+	return _prep_start_time;
 }
 
-int foodItem::status()
+int FoodItem::relinquish_time()
+{
+	return _relinquish_time;
+}
+
+int FoodItem::status()
 {
 	return _status;
 }
 
-int foodItem::cID()
+int FoodItem::customer_id()
 {
-	return _cID;
+	return _customer_id;
 }
 
 /******************************************************************************/
 /********************************* SETTERS ************************************/
 /******************************************************************************/
 
-void foodItem::orderT(int orderT)
+void FoodItem::order_time(int order_time)
 {
-	_orderT = orderT;
+	_order_time = order_time;
 }
 
-void foodItem::delivT(int delivT)
+void FoodItem::delivery_time(int delivery_time)
 {
-	_delivT = delivT;
+	_delivery_time = delivery_time;
 }
 
-void foodItem::prepStrT(int prepStrT)
+void FoodItem::prep_start_time(int prep_start_time)
 {
-	_prepStrT = prepStrT;
+	_prep_start_time = prep_start_time;
 }
 
-void foodItem::prepEndT(int prepEndT)
+void FoodItem::prep_end_time(int prep_end_time)
 {
-	_prepEndT = prepEndT;
+	_prep_end_time = prep_end_time;
 }
 
-void foodItem::relinquishT(int relinquishT)
+void FoodItem::relinquish_time(int relinquish_time)
 {
-	_relinquishT = relinquishT;
+	_relinquish_time = relinquish_time;
 }
 
-void foodItem::status(int status)
+void FoodItem::status(int status)
 {
 	_status = status;
 }
 
-void foodItem::cID(int cID)
+void FoodItem::customer_id(int customer_id)
 {
-	_cID = cID;
+	_customer_id = customer_id;
 }
+
 /******************************************************************************/
 /**************************** WRITE FUNCTIONS *********************************/
 /******************************************************************************/
 
-void foodItem::write(ostream &out)
+void FoodItem::Write(ostream &out)
 {
 	out << "Food Item" << endl;
-	out << "\tFood ID:                " << _id                    << endl;
-	out << "\tFood Type:              " << _fType                 << endl;
-	out << "\tOrder Kit Time:         " << _orderT      << " min" << endl;
-	out << "\tDelivered Kit Time:     " << _delivT      << " min" << endl;
-	out << "\tStart Preparation Time: " << _prepStrT    << " min" << endl;
-	out << "\tEnd Preparation Time:   " << _prepEndT    << " min" << endl;
-	out << "\tSold/Discarded Time:    " << _relinquishT << " min" << endl;
-	out << "\tSold/Discarded:         " << _status      << " min" << endl;
-	out << "\tCustomer ID Bought :    " << _cID                   << endl;
+	out << "\tFood ID:                " << _food_id                   << endl;
+	out << "\tFood Type:              " << _food_type                 << endl;
+	out << "\tOrder Kit Time:         " << _order_time      << " min" << endl;
+	out << "\tDelivered Kit Time:     " << _delivery_time   << " min" << endl;
+	out << "\tStart Preparation Time: " << _prep_start_time << " min" << endl;
+	out << "\tEnd Preparation Time:   " << _prep_end_time   << " min" << endl;
+	out << "\tSold/Discarded Time:    " << _relinquish_time << " min" << endl;
+	out << "\tSold/Discarded:         " << _status          << " min" << endl;
+	out << "\tCustomer ID Bought :    " << _customer_id               << endl;
 }
 
-void foodItem::write_Rpt(ostream &out)
+void FoodItem::WriteReport(ostream &out)
 {
-	out << _id          << " " << _fType    << " " << _orderT   << " "
-	    << _delivT      << " " << _prepStrT << " " << _prepEndT << " "
-	    << _relinquishT << " " << _status   << " " << endl;
+	out << _food_id         << " " << _food_type       << " " << _order_time    << " "
+	    << _delivery_time   << " " << _prep_start_time << " " << _prep_end_time << " "
+	    << _relinquish_time << " " << _status          << " " << endl;
 }
